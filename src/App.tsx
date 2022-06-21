254d4de4df51af5b5b396640336157c110ee4a68
createobjext <screamless@gmail.com>
2022-06-21 17:40:49 +0300

    commit 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+/*function App() {
   return (
     <div className="App">
      Hello, samurai! Let's go!
     </div>
   );
+}*/
+
+const App = () => {
+    return (
+        <div className='App'>
+            <h1> Catarpillar </h1>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    )
 }
 
 export default App;
